7f47dfda87b1ff2c9c40512d686259846312d745
victorsootho <soothotsoenyane@gmail.com>
2023-01-06 08:17:39 +0200

    Initial Commit

---
+++ pages/components/Hangword.tsx
@@ -0,0 +1,11 @@
+const Hangword = () => {
+  return (
+    <div>
+      <div>
+        <h3 className="text-xl font-semibold">HangWord</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hangword;
